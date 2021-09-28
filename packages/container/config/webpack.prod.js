ae4a2f4da8105f453b99be5ae8793f2073411764
Sergey <mrmozolevsky@gmail.com>
2021-09-28 21:15:35 +0200

    Added marketing deployment pipeline

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const productionConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies,
         })
